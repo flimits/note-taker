a017df89187dafe63bc29fa929a7a21bb59eb4d7
Jason Savoie <flimits@gmail.com>
2023-10-27 21:36:33 -0700

    got a review to actually save in file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./public/assets/js/index');
-const PORT = 3001;
+const fs = require('fs');
+const PORT = process.env.port || 3001;
+
 const app = express();
 const db = require('./db/db.json');
-const { publicDecrypt } = require('crypto');
+// const { publicDecrypt } = require('crypto');
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -19,7 +20,47 @@ app.get("/", function (req, res) {
 app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, 'db/db.json'));
     console.log(`${req.method} this is api call fo notes`)
-})
+});
+
+app.post("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, 'db/db.json'));
+    console.log(`${req.method} this is api call fo notes`);
+
+
+    const { title, text } = req.body;
+
+    const newNote = {
+        title,
+        text
+    };
+
+    // Obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+
+            // Let's add a new note
+            parsedNotes.push(newNote);
+
+            db.push(newNote);
+
+            // Update the note db with latest note added.
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(parsedNotes, null, 4),
+                (writeErr) =>
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.info('Successfully updated note!')
+            );
+        }
+    });
+
+
+
+});
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
