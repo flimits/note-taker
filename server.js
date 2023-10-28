94cc296a0e90a7527f62aab1a8f03bc757a7478c
Jason Savoie <flimits@gmail.com>
2023-10-27 22:58:40 -0700

    fixing port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = process.env.port || 32309;
+const PORT = process.env.PORT || 3001;
+
 
 const app = express();
 const db = require('./db/db.json');
